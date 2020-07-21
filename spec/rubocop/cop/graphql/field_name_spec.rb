e4f2915bdfbf5386033addf3ca4ca9c861a1b71b
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2020-07-21 16:08:57 -0400

    Fix NodePattern for fields without a name (#20)
    `field` can be called without a position name argument which causes
    errors right now. This also happens when FieldExtension's are used;
    methods are called on `field` instead of passing arguments.
    
    The simplest fix was to ensure the node pattern matching for field
    definitions required a symbol positional argument (the field name).

--- spec/rubocop/cop/graphql/field_name_spec.rb
+++ spec/rubocop/cop/graphql/field_name_spec.rb
@@ -15,6 +15,28 @@ RSpec.describe RuboCop::Cop::GraphQL::FieldName do
     end
   end
 
+  context "when field is called on an extension" do
+    it "not registers an offense" do
+      expect_no_offenses(<<~RUBY)
+        class MyExtension < GraphQL::Schema::FieldExtension
+          def apply
+            field.argument(:width, Integer, required: false)
+          end
+        end
+      RUBY
+    end
+  end
+
+  context "when field has no name" do
+    it "not registers an offense" do
+      expect_no_offenses(<<~RUBY)
+        class UserType < BaseType
+          field field: MyField
+        end
+      RUBY
+    end
+  end
+
   context "when field name is in camel case" do
     it "registers an offense" do
       expect_offense(<<~RUBY)
