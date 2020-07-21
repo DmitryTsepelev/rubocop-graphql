dce9623ac3a901ce5908b52096144d1352e0bfb8
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2020-07-21 13:21:06 -0400

    Fix NodePattern for fields without a name
    `field` can be called without a position name argument which causes
    errors right now. This also happens when FieldExtension's are used;
    methods are called on `field` instead of passing arguments.
    
    The simplest fix was to ensure the node pattern matching for field
    definitions required a symbol positional argument (the field name).

--- lib/rubocop/graphql/node_pattern.rb
+++ lib/rubocop/graphql/node_pattern.rb
@@ -6,18 +6,18 @@ module RuboCop
       extend RuboCop::NodePattern::Macros
 
       def_node_matcher :field_definition?, <<~PATTERN
-        (send nil? :field ...)
+        (send nil? :field (:sym _) ...)
       PATTERN
 
       def_node_matcher :field_definition_with_body?, <<~PATTERN
         (block
-          (send nil? :field ...)
+          (send nil? :field (:sym _) ...)
           ...
         )
       PATTERN
 
       def_node_matcher :argument?, <<~PATTERN
-        (send nil? :argument ...)
+        (send nil? :argument (:sym _) ...)
       PATTERN
 
       def field?(node)
