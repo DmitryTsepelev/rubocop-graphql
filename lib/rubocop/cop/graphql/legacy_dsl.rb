c4b884ca76db9b4b05cd99437ce91cefe01db20c
Daniel Dao <daniel.vu.dao@gmail.com>
2021-05-15 22:38:08 -0700

    [Add] Use class-based definitions

---
+++ lib/rubocop/cop/graphql/legacy_dsl.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module GraphQL
+    # Matches the legacy DSL object behavior for GraphQL 1.8x and below
+    # Example = GraphQL::ObjectType.define do
+    #   ....
+    #   ....
+    # end
+    module Cop
+      class LegacyDsl < Base
+        include RuboCop::GraphQL::NodePattern
+
+        MSG = 'Avoid using legacy based type-based definitions. Use class-based defintions instead.'
+
+        def on_send(node)
+          return unless legacy_dsl?(node)
+        end
+      end
+    end
+  end
+end
