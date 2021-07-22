fd94b802cc09cdfcc2cf0bfda8d444e3bc467639
Jason Draper <jason@drapergeek.com>
2021-07-22 13:58:00 -0400

    Use refinements to maybe add String#underscore

--- lib/rubocop/cop/graphql/field_name.rb
+++ lib/rubocop/cop/graphql/field_name.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require_relative "../../../refinements/underscore_string"
+
+using UnderscoreString unless String.method_defined?(:underscore)
+
 module RuboCop
   module Cop
     module GraphQL
@@ -41,25 +45,8 @@ module RuboCop
 
         def rename_field_name(corrector, field, node)
           name_field = field.name.to_s
-          new_line = node.source.sub(name_field, underscore(name_field))
+          new_line = node.source.sub(name_field, name_field.underscore)
           corrector.replace(node, new_line)
-        end
-
-        # This method was extracted from activesupport in Rails:
-        # https://github.com/rails/rails/blob/8dab534ca81dd32c6a83ac03596a1feb7ddaaca7/activesupport/lib/active_support/inflector/methods.rb#L96
-
-        def underscore(camel_cased_word)
-          regex = /(?:(?<=([A-Za-z\d]))|\b)((?=a)b)(?=\b|[^a-z])/
-          return camel_cased_word.to_s unless /[A-Z-]|::/.match?(camel_cased_word)
-
-          word = camel_cased_word.to_s.gsub("::", "/")
-          word.gsub!(regex) { "#{Regexp.last_match(1) && '_'}#{Regexp.last_match(2).downcase}" }
-          word.gsub!(/([A-Z\d]+)(?=[A-Z][a-z])|([a-z\d])(?=[A-Z])/) do
-            (Regexp.last_match(1) || Regexp.last_match(2)) << "_"
-          end
-          word.tr!("-", "_")
-          word.downcase!
-          word
         end
       end
     end
