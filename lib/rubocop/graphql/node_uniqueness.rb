c4cbe594d7a7d96a9085534a0d17b976c631a7cd
Nikita Vasilevsky <nvasilevskii@gmail.com>
2022-01-07 20:14:59 +0000

    [Cleanup] Remove unused NodeUniqueness#nested_class? method

--- lib/rubocop/graphql/node_uniqueness.rb
+++ lib/rubocop/graphql/node_uniqueness.rb
@@ -7,10 +7,6 @@ module RuboCop
       def current_class_full_name(node)
         node.each_ancestor(:class).map(&:defined_module_name).join("::")
       end
-
-      def nested_class?(node)
-        node.each_ancestor(:class).any?
-      end
     end
   end
 end
