97c75485e78b4c725557a2cc509263aa837de09c
Nikita Vasilevsky <nikita.vasilevsky@shopify.com>
2022-01-07 13:25:38 -0500

    Scope definitions per full namespace (#69)

--- lib/rubocop/graphql/node_uniqueness.rb
+++ lib/rubocop/graphql/node_uniqueness.rb
@@ -4,8 +4,12 @@ module RuboCop
   module GraphQL
     # Shared methods to check duplicated definitions
     module NodeUniqueness
-      def current_class_name(node)
-        node.each_ancestor(:class).first.defined_module_name
+      def current_class_full_name(node)
+        node.each_ancestor(:class).map(&:defined_module_name).join("::")
+      end
+
+      def nested_class?(node)
+        node.each_ancestor(:class).any?
       end
     end
   end
