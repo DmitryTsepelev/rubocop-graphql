4ba36a80f8bc82a9d2d4b7903fced2431778e9c6
Alex Chan <alex.chan@shopify.com>
2022-03-21 19:11:54 -0400

    [FieldDefinitions] Support heredoc descriptions

---
+++ lib/rubocop/graphql/heredoc.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module GraphQL
+    module Heredoc
+      def heredoc?(node)
+        (node.str_type? || node.dstr_type?) && node.heredoc?
+      end
+
+      def range_including_heredoc(node)
+        field = RuboCop::GraphQL::Field.new(node)
+        last_heredoc = field.kwargs.instance_variable_get(:@nodes).reverse.find do |kwarg|
+                         heredoc?(kwarg.value)
+                       end&.value
+
+        range = node.loc.expression
+        range = range.join(last_heredoc.loc.heredoc_end) if last_heredoc
+
+        range_by_whole_lines(range)
+      end
+    end
+  end
+end
