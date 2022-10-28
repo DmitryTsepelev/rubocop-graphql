87a21cccdba60b63a843dc9b1110985534cdd38c
Daniel Dao <daniel.dao@chime.com>
2022-10-28 15:25:49 -0700

    🚧

--- spec/rubocop/cop/graphql/unnecessary_argument_camelize_spec.rb
+++ spec/rubocop/cop/graphql/unnecessary_argument_camelize_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe RuboCop::Cop::GraphQL::UnnecessaryArgumentCamelize, :config do
 
   context "when unnecessary argument camelize defined" do
     it "registers an offense" do
-      expect_no_offenses(<<~RUBY)
+      expect_offenses(<<~RUBY)
         class UserType < BaseType
           field :first_name, String, null: true do
             argument :foo, String, required: true, camelize: false
