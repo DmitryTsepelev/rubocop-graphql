0b81abd0701d08b6ffa8d14c2cf1cb6a27e90c82
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2023-02-13 18:54:31 +0300

    Add NotAuthorizedNodeType cop

---
+++ spec/rubocop/cop/graphql/not_authorized_node_type_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::GraphQL::NotAuthorizedNodeType, :config do
+  context "when type not implements Node interface" do
+    specify do
+      expect_no_offenses(<<~RUBY, "graphql/types/user_type.rb")
+        class UserType < BaseType
+          field :uuid, ID, null: false
+        end
+      RUBY
+    end
+  end
+
+  context "when type not implements Node interface" do
+    context "when type has .authorized? check" do
+      specify do
+        expect_no_offenses(<<~RUBY, "graphql/types/user_type.rb")
+          class UserType < BaseType
+            implements GraphQL::Types::Relay::Node
+
+            field :uuid, ID, null: false
+
+            def self.authorized?(object, context)
+              super && object.owner == context[:viewer]
+            end
+          end
+        RUBY
+      end
+    end
+
+    context "when type has no .authorized? check" do
+      specify do
+        expect_offense(<<~RUBY, "graphql/types/user_type.rb")
+          class UserType < BaseType
+          ^^^^^^^^^^^^^^^^^^^^^^^^^ .authorized? should be defined for types implementing Node interface.
+            implements GraphQL::Types::Relay::Node
+
+            field :uuid, ID, null: false
+          end
+        RUBY
+      end
+    end
+  end
+end
