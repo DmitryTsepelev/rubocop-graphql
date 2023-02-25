11d89457abfd633c9e04913b39d9b9f0f09a5c43
Daniel Dao <danielvdao@users.noreply.github.com>
2023-02-25 04:25:22 -0800

    🐛  `GraphQL/OrderedFields` doesn't behave correctly when there are spaces or blocks in between (#111)

--- spec/rubocop/cop/graphql/ordered_fields_spec.rb
+++ spec/rubocop/cop/graphql/ordered_fields_spec.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::GraphQL::OrderedFields, :config do
+  let(:config) do
+    RuboCop::Config.new(
+      "GraphQL/OrderedFields" => {
+        "Groups" => true
+      }
+    )
+  end
+
+  context "when there is a block" do
+    it "registers an offense" do
+      expect_offense(<<~RUBY)
+        class UserType < BaseType
+          field :birthdate, Int, null: true do
+            description 'foo'
+          end
+          field :abc, String, null: true
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Fields should be sorted in an alphabetical order within their section. Field `abc` should appear before `birthdate`.
+        end
+      RUBY
+    end
+  end
+
   context "when fields are alphabetically sorted" do
     it "not registers an offense" do
       expect_no_offenses(<<~RUBY)
@@ -30,8 +52,8 @@ RSpec.describe RuboCop::Cop::GraphQL::OrderedFields, :config do
     end
   end
 
-  context "when each individual groups are alphabetically sorted" do
-    it "not registers an offense" do
+  context "when grouped fields are not alphabetically sorted" do
+    it "does not register an offense" do
       expect_no_offenses(<<~RUBY)
         class UserType < BaseType
           field :birthdate, Int, null: true
@@ -104,6 +126,64 @@ RSpec.describe RuboCop::Cop::GraphQL::OrderedFields, :config do
           field :phone, String, null: true
         end
       RUBY
+    end
+  end
+
+  context "when group config is false" do
+    let(:config) do
+      RuboCop::Config.new(
+        "GraphQL/OrderedFields" => {
+          "Groups" => false
+        }
+      )
+    end
+
+    context "when there are blocks" do
+      it "registers an offense" do
+        expect_offense(<<~RUBY)
+          class UserType < BaseType
+            field :phone, String, null: true do
+              argument :something, String, required: false
+            end
+            field :name, String, null: true do
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Fields should be sorted in an alphabetical order within their section. Field `name` should appear before `phone`.
+            end
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          class UserType < BaseType
+            field :name, String, null: true do
+            end
+            field :phone, String, null: true do
+              argument :something, String, required: false
+            end
+          end
+        RUBY
+      end
+    end
+
+    context "when there are no blocks" do
+      it "registers an offense" do
+        expect_offense(<<~RUBY)
+          class UserType < BaseType
+            field :phone, String, null: true
+
+
+            field :name, String, null: true
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Fields should be sorted in an alphabetical order within their section. Field `name` should appear before `phone`.
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          class UserType < BaseType
+            field :name, String, null: true
+            field :phone, String, null: true
+
+
+          end
+        RUBY
+      end
     end
   end
 
